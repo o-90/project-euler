97cb6d65ce2e4ec452490e405c11ce887347f55d
John <john.r.martinez14@gmail.com>
2016-01-10 19:39:10 -0600

    #13 and #14

--- one_hundred.py
+++ one_hundred.py
@@ -237,3 +237,30 @@ r = """
 53503534226472524250874054075591789781264330331690"""
 
 str(sum([int(x) for x in r.split("\n")]))[:10]
+
+# 14)
+# The following iterative sequence is defined for the set of positive integers:
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+
+# Using the rule above and starting with 13, we generate the following sequence:
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains
+# 10 terms. Although it has not been proved yet (Collatz Problem), it is thought
+# that all starting numbers finish at 1.
+
+# Which starting number, under one million, produces the longest chain?
+
+def collatz(n, c=0):
+    if n < 2:
+	return c
+    else:
+	if n % 2 == 0:
+	    n /= 2
+	else:
+	    n = 3*n + 1
+
+    return collatz(n, c+1)
+
+999999 - np.argmax([collatz(i) for i in xrange(999999, 1, -1)])
