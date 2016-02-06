c188ad8d11407afb749a791bf298587a9d102a07
GoBrewers14 <john.r.martinez14@gmail.com>
2016-02-06 15:28:01 -0600

    updated problem 104

--- 101-200/tens.py
+++ 101-200/tens.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 
+
 from itertools import permutations
 from collections import defaultdict
+
+
+# 104)
+# The Fibonacci sequence is defined by the recurrence relation:
+#    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+#
+# It turns out that F541, which contains 113 digits, is the first
+# Fibonacci number for which the last nine digits are 1-9 pandigital
+# (contain all the digits 1 to 9, but not necessarily in order). And F2749,
+# which contains 575 digits, is the first Fibonacci number for which the
+# first nine digits are 1-9 pandigital.  Given that Fk is the first Fibonacci
+# number for which the first nine digits AND the last nine digits
+# are 1-9 pandigital, find k.
+
 
 d = defaultdit(int)
 
@@ -20,3 +35,5 @@ for k in xrange(1000000):
         if int(str(a)[::-1]) % 1000000000 in d:
             print k + 1
             break
+
+
