a46643122b859bb9b26e2fd425141b72826cc016
GoBrewers14 <john.r.martinez14@gmail.com>
2016-02-05 22:11:08 -0600

    rearranged dir structure; finished problem #74

--- 1-100/20.py
+++ 1-100/20.py
@@ -1,139 +1,3 @@
-# -*- coding: utf-8 -*-
-
-import numpy as np
-import itertools
-from math import sqrt
-
-# 1)
-# Find the sum of all the multiples of 3 or 5 below 1000
-sum([x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0])
-
-# 2)
-# Find the sum of the even-valued terms in the Fibonacci sequence
-# whose value doesn't exceed 4,000,000
-
-fibs = [1,1]
-
-while fibs[-1] < 4000000:
-    fibs.append(fibs[-1]+fibs[-2])
-
-sum([x for x in fibs if x % 2 == 0])
-
-# 3)
-# What is the largest prime factor of the number 600851475143 ?
-
-def find_primes(n):
-    threshold = int(np.ceil(sqrt(n)))
-    for x in xrange(threshold, 0, -1):
-	if i % x == 0:
-	    break
-
-    if x == 1:
-        return i
-    else:
-        return find_primes(x), find_primes(i / x)
-
-max(find_primes(600851475143))
-
-# 4)
-# Find the largest palindrome made from the product of two
-# 3-digit numbers
-
-# 5)
-# What is the smallest positive number that is evenly divisible by
-# all the numbers from 1 - 20 ?
-
-# 6)
-# Find the difference between the sum of the squares of the first
-# 100 natural numners and the square of the sum.
-
-(100*101 / 2)**2 - sum([x**2 for x in xrange(1,101)])
-
-# 7)
-# What is the 10001st prime number?
-
-
-# 8)
-# The four adjacent digits in the 1000-digit number that have the 
-# greatest product are 9 * 9 * 8 * 9 = 5832
-
-'''
-73167176531330624919225119674426574742355349194934
-96983520312774506326239578318016984801869478851843
-85861560789112949495459501737958331952853208805511
-12540698747158523863050715693290963295227443043557
-66896648950445244523161731856403098711121722383113
-62229893423380308135336276614282806444486645238749
-30358907296290491560440772390713810515859307960866
-70172427121883998797908792274921901699720888093776
-65727333001053367881220235421809751254540594752243
-52584907711670556013604839586446706324415722155397
-53697817977846174064955149290862569321978468622482
-83972241375657056057490261407972968652414535100474
-82166370484403199890008895243450658541227588666881
-16427171479924442928230863465674813919123162824586
-17866458359124566529476545682848912883142607690042
-24219022671055626321111109370544217506941658960408
-07198403850962455444362981230987879927244284909188
-84580156166097919133875499200524063689912560717606
-05886116467109405077541002256983155200055935729725
-71636269561882670428252483600823257530420752963450
-'''
-# Find the thirteen adjacent digits in the 1000-digit number that
-# have the greatest product. What is the value of this product?
-
-prods = []
-r = r.replace("\n", "")
-
-for i in xrange(1000 - 13 + 1):
-    tmp = r[i:i+13]
-    p = np.prod([int(char) for char in tmp])
-    prods.append(p)
-
-max(prods)
-
-# 9)
-# A Pythagorean triplet is a set of three natural numbers, a < b < c,
-# for which, a**2 + b**2 = c**2
-# There exists exactly one Pythagorean triple for which a + b + c = 1000
-# Find a*b*c
-
-lst = []
-for c in xrange(1,1001):
-    for b in xrange(c):
-        for a in xrange(b):
-	    if a ** 2 + b ** 2 == c ** 2 and a + b + c == 1000:
-		lst.append((a, b, c))
-		break
-
-print np.prod(lst)
-
-# 10)
-# Find the sum of the primes less than 2000000
-
-def erat2():
-    """
-    from http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n
-    """
-    d = {}
-    yield 2
-
-    for q in itertools.islice(itertools.count(3), 0, None, 2):
-        p = d.pop(q, None)
-	if p is None:
-	    d[q*q] = q
-	    yield q
-	else:
-	    x = p + q
-	        while x in d or not (x&1):
-		    x += p
-	        d[x] = p
-
-def get_primes_erat(n):
-    return list(itertools.takewhile(lambda p: p < n, erat2()))
-
-print sum(get_primes_erat(2000000))
-
 # 11)
 #
 
@@ -264,12 +128,12 @@ str(sum([int(x) for x in r.split("\n")]))[:10]
 
 def collatz(n, c=0):
     if n < 2:
-	return c
+        return c
     else:
-	if n % 2 == 0:
-	    n /= 2
-	else:
-	    n = 3*n + 1
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3*n + 1
 
     return collatz(n, c+1)
 
---
+++ 1-100/80.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import time
+from collections import defaultdict
+
+# 74)
+# The number 145 is well known for the property that the sum of the factorial of its digits is equal to 145:
+#    1! + 4! + 5! = 1 + 24 + 120 = 145
+# Perhaps less well known is 169, in that it produces the longest chain of numbers that link back to 169; it turns ou$
+#    169 → 363601 → 1454 → 169
+#    871 → 45361 → 871
+#    872 → 45362 → 872
+# It is not difficult to prove that EVERY starting number will eventually get stuck in a loop. For example,
+#    69 → 363600 → 1454 → 169 → 363601 (→ 1454)
+#    78 → 45360 → 871 → 45361 (→ 871)
+#    540 → 145 (→ 145)
+# Starting with 69 produces a chain of five non-repeating terms, but the longest non-repeating chain with a starting $
+# How many chains, with a starting number below one million, contain exactly sixty non-repeating terms?
+
+# helper functions
+def _fac(n):
+    if n == 0:
+        return 1
+    else:
+        return n * _fac(n - 1)
+
+def _parse_nums(n):
+    return [int(x) for x in str(n)]
+
+def _get_new_num(n):
+    return sum(map(fac, _parse_nums(n)))
+
+
+st = time.time()
+c = 0
+
+for i in xrange(1000000):
+    # do updates for shits and gigs
+    if (i + 1) % 100000 == 0 and i > 0:
+        et = time.time()
+        print "c-value ;= {}\t@ iter := {}\ttimeelapsed := {}".format(c, i+1, et-st)
+
+    d = defaultdict(int)
+    lst = [i]
+
+    while d.get(lst[-1], None) is None:
+        d[lst[-1]] += 1
+        n = _get_new(lst[-1])
+        lst.append(n)
+
+    if len(lst) - 1 == 60:
+        c += 1
+
+# c-value := 402
