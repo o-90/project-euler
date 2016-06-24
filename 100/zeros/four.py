d8e49401aecd90b0f1ef39833ec047420c00013b
John Martinez <john.r.martinez14@gmail.com>
2016-06-24 11:30:20 -0500

    completed #4 but hadn't added code yet.

--- 100/zeros/four.py
+++ 100/zeros/four.py
@@ -3,3 +3,14 @@
 # 4)
 # Find the largest palindrome made from the product of two
 # 3-digit numbers
+
+tmp = []
+
+for i in xrange(100, 1000):
+    for j in xrange(100, 1000):
+        prod = i*j
+        if str(prod) == str(prod)[::-1]:
+            tmp.append(prod)
+
+ans = max(tmp)
+print ans  # 906609
