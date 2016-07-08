00bdfcaa4d42d10b39702c301b41294a2d439d2a
GoBrewers14 <john.r.martinez14@gmail.com>
2016-07-07 20:34:41 -0500

    #31 added answer

--- 100/thirties/thirty-one.hs
+++ 100/thirties/thirty-one.hs
@@ -25,4 +25,4 @@ numOfCoins money coins@(x:xs)    = numOfCoins money xs + numOfCoins (money-x) co
 main :: IO ()
 main = do
   let ans = numOfCoins 200 [1, 2, 5, 10, 20, 50, 100, 200]
-  print ans
+  print ans  -- 73682
