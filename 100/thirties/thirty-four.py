5b53d9f15d10c5834eb02971d5e8ff8613207fcd
John Martinez <john.r.martinez14@gmail.com>
2016-07-11 13:43:11 -0500

    fixed indentations.  Added question doc.

--- 100/thirties/thirty-four.py
+++ 100/thirties/thirty-four.py
@@ -2,13 +2,21 @@
 
 from math import factorial
 
-facts = [factorial(x) for x in xrange(0, 10)]
+
+# 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+#
+# Find the sum of all numbers which are equal to the sum of the factorial
+# of their digits.
+#
+# Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
 
 c = 0
+facts = [factorial(x) for x in xrange(0, 10)]
 
 for i in xrange(3, 100000):
-	if i == sum(facts[int(d)] for d in str(i)):
-		c += i
+    if i == sum(facts[int(d)] for d in str(i)):
+        c += i
 
 ans = c
 print ans  # 40730
