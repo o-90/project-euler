dcc1b2f6f6257f6e1fab752cc5bb71ef42eb28e5
gobrewers14 <john.r.martinez14@gmail.com>
2016-07-18 13:09:02 -0500

    finished 98

--- 100/nineties/ninety-eight.py
+++ 100/nineties/ninety-eight.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
-
+from math import sqrt, floor, ceil
 
 # 98)
 # By replacing each of the letters in the word CARE with 1, 2, 9, and 6
@@ -17,43 +17,68 @@ from collections import defaultdict
 #
 # What is the largest square number formed by any member of such a pair?
 
+# NOTE:  If we contruct a set of available anagrams from the word doc
+# provided, we can see that there are anagram pairs that are 9, 8, 6, 5,
+# 4, 3, and 2 characters long.  So we will need all perfect squares of
+# length 9, 8, 6, 5, 4, 3, and 2 that share the same digits with another
+# square.
 
 
-
-def num_ranges(n):
+def is_anagram(a, b):
     """
-    only get squares that are of length 10 or less.
+    check if given two strings are anagrams
     """
-    if len(str(n)) < 11:
-        return True
-    else:
-        False
+    if a != b:
+        return sorted(a) == sorted(b)
+    return False
 
+def contains_zeros(n):
+    """
+    check if a square contains zeroes
+    """
+    n = str(n) if not isinstance(n, str) else n
+    return any(x for x in n if x == '0')
 
-def is_anagram(x, y):
+def word_permute(n, m):
     """
-    returns T/F whether two strings are anagrams or not
+    given a word has a mapping 0-9 for its characters
+    return the permutation of the original words index
+    given a second word.
     """
-    return sorted(x) == sorted(y)
+    n = str(n) if not isinstance(n, str) else n
+    m = str(m) if not isinstance(m, str) else m
+    return ''.join(str(n.index(x)) for x in m)
 
 # import words
 f = open("../../data/p098_words.txt", 'r')
 text = f.readlines()[0]
 word_list = text.replace('"', '').strip().split(',')
 
-# create word graph
-word_graph = defaultdict(list)
-for e, word_1 in enumerate(word_list):
-    for word_2 in word_list[(e+1):]:
-        if is_anagram(word_1, word_2):
-            word_graph[word_1].append(word_2)
+dic = defaultdict(list)
 
+for e, word0 in enumerate(word_list):
+    for word1 in word_list[e:]:
+        if is_anagram(word0, word1):
+                dic[len(word0)].append((word0, word1))
 
-#squares = [n**2 for n in xrange(100000) if num_ranges(n**2)]
+word_lens = sorted(dic.keys())
+ans = []
 
+for word_len in word_lens[:-2]:
+    lower = int(floor(sqrt(10**(word_len-1))))
+    upper = int(ceil(sqrt(10**(word_len))))
+    squares_to_check = [str(x*x) for x in xrange(lower, upper) if not contains_zeros(x*x)]
+    anagrams = dic[word_len]
+    for anagram in anagrams:
+        perm = word_permute(anagram[0], anagram[1])
+        for k, sq_0 in enumerate(squares_to_check):
+            for sq_1 in squares_to_check[(k+1):]:
+                try:
+                    if word_permute(sq_0, sq_1) == perm:
+                        ans.append(max(int(sq_0), int(sq_1)))
+                        break
+                except ValueError:
+                    continue
 
-#int(''.join([x for x, y in sorted(zip('reduction', '685072341'), key=lambda x: x[1])]))
-
-
-
-
+ans = max(ans)
+print ans  # 18769
