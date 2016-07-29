51b003ab86cdae4fa3bfedd70c531de5b7f44dd8
John Martinez <john.r.martinez14@gmail.com>
2016-07-29 08:06:19 -0500

    indentation clean-up on three

--- 100/zeros/three.py
+++ 100/zeros/three.py
@@ -5,15 +5,16 @@ from __future__ import division
 # 3)
 # What is the largest prime factor of the number 600851475143 ?
 
+
 def find_max_prime(n):
-	i = 2
-	while i * i < n:
-		while n % i == 0:
-			n = n / i
+    i = 2
+    while i * i < n:
+        while n % i == 0:
+	    n = n / i
 
-		i = i + 1
+	i = i + 1
 
-	return int(n)
+    return int(n)
 
 ans = find_max_prime(600851475143)
 print ans  # 6857
